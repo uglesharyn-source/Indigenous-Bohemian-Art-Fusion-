5263312c8f3dd132f1e88d60e73a0ed758e8574b
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 09:05:45 +1000

    Initialize carlosmuvi/fc-maestro-livemode

---
+++ scripts/execute_maestro_tests.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -o pipefail
+set -x
+
+now=$(date +%F_%H-%M-%S)
+echo $now
+
+# Install Maestro
+export MAESTRO_VERSION=1.21.3; curl -Ls "https://get.maestro.mobile.dev" | bash
+export PATH="$PATH":"$HOME/.maestro/bin"
+maestro -v
+
+# Compile and install APK.
+./gradlew -PSTRIPE_FINANCIAL_CONNECTIONS_EXAMPLE_BACKEND_URL=$STRIPE_FINANCIAL_CONNECTIONS_EXAMPLE_BACKEND_URL :financial-connections-example:installDebug
+
+# Start screen record (adb screenrecord has a 3 min limit).
+adb shell "screenrecord /sdcard/$now-1.mp4; screenrecord /sdcard/$now-2.mp4" &
+
+# Store the process ID
+childpid=$!
+
+echo "Executing maestro tests"
+maestro test -e APP_ID=com.stripe.android.financialconnections.example --format junit --output maestroReport.xml maestro/financial-connections-livemode/
+result=$?
+
+echo "Waiting for record to finish"
+kill -2 "$childpid"
+wait "$childpid"
+
+# Sleep for a short duration to allow the process to finalize the video file
+sleep 3
+
+echo "Pulling video results from device."
+mkdir -p /tmp/test_results
+cd /tmp/test_results
+adb pull "/sdcard/$now-1.mp4" || true
+adb pull "/sdcard/$now-2.mp4" || true
+
+exit "$result"
