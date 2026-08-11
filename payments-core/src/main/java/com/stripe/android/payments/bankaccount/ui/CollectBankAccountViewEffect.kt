910a942ef0a8c71a760e5e76f6e72225ddf44ef8
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 09:38:22 +1000

    Initialize jameswoo/customersheet-payment-element-12

---
+++ payments-core/src/main/java/com/stripe/android/payments/bankaccount/ui/CollectBankAccountViewEffect.kt
@@ -0,0 +1,28 @@
+package com.stripe.android.payments.bankaccount.ui
+
+import com.stripe.android.payments.bankaccount.navigation.CollectBankAccountResultInternal
+
+/**
+ * Used communicate view side-effects from [CollectBankAccountViewModel]
+ * to [CollectBankAccountActivity] following the Unidirectional Data Flow conventions.
+ *
+ * Any one-off events should be communicated to the view via an instance of this sealed class.
+ */
+internal sealed class CollectBankAccountViewEffect {
+
+    /**
+     * Instruct the view to open the financial connections SDK flow.
+     */
+    data class OpenConnectionsFlow(
+        val publishableKey: String,
+        val financialConnectionsSessionSecret: String,
+        val stripeAccountId: String?
+    ) : CollectBankAccountViewEffect()
+
+    /**
+     * Instruct the view to finish with the given [CollectBankAccountResultInternal].
+     */
+    data class FinishWithResult(
+        val result: CollectBankAccountResultInternal
+    ) : CollectBankAccountViewEffect()
+}
