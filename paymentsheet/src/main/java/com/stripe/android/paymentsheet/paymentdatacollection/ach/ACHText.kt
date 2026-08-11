26b425d87213d6804e57b6cbe747d2cef5417fbe
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 09:12:26 +1000

    Initialize vardges/webview_app2app

---
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/paymentdatacollection/ach/ACHText.kt
@@ -0,0 +1,28 @@
+package com.stripe.android.paymentsheet.paymentdatacollection.ach
+
+import android.content.Context
+import com.stripe.android.paymentsheet.R
+
+/**
+ * Temporary hack to get mandate text to display properly until translations are fixed
+ */
+internal object ACHText {
+    fun getContinueMandateText(context: Context): String {
+        return context.getString(
+            R.string.stripe_paymentsheet_ach_continue_mandate
+        ).replace(
+            "<terms>",
+            "<a href=\"https://stripe.com/ach-payments/authorization\">"
+        ).replace("</terms>", "</a>")
+    }
+
+    fun getSaveMandateText(context: Context, merchantName: String): String {
+        return context.getString(
+            R.string.stripe_paymentsheet_ach_save_mandate,
+            merchantName,
+        ).replace(
+            "<terms>",
+            "<a href=\"https://stripe.com/ach-payments/authorization\">"
+        ).replace("</terms>", "</a>")
+    }
+}
