d2ca129d4903e1102a2754ed8536b984e407993e
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 08:50:23 +1000

    Initialize jameswoo/ume-pre-alpha

---
+++ identity/src/main/java/com/stripe/android/identity/navigation/ConfirmationFragment.kt
@@ -0,0 +1,64 @@
+package com.stripe.android.identity.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.stripe.android.identity.IdentityVerificationSheet
+import com.stripe.android.identity.VerificationFlowFinishable
+import com.stripe.android.identity.analytics.IdentityAnalyticsRequestFactory.Companion.SCREEN_NAME_CONFIRMATION
+import com.stripe.android.identity.ui.ConfirmationScreen
+import com.stripe.android.identity.viewmodel.IdentityViewModel
+import kotlinx.coroutines.launch
+
+/**
+ * Fragment for confirmation.
+ */
+internal class ConfirmationFragment(
+    private val identityViewModelFactory: ViewModelProvider.Factory,
+    private val verificationFlowFinishable: VerificationFlowFinishable
+) : Fragment() {
+
+    private val identityViewModel: IdentityViewModel by activityViewModels {
+        identityViewModelFactory
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = ComposeView(requireContext()).apply {
+        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+        setContent {
+            val verificationPage by identityViewModel.verificationPage.observeAsState()
+            ConfirmationScreen(verificationPage) {
+                identityViewModel.sendSucceededAnalyticsRequestForNative()
+                verificationFlowFinishable.finishWithResult(
+                    IdentityVerificationSheet.VerificationFlowResult.Completed
+                )
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch(identityViewModel.workContext) {
+            identityViewModel.screenTracker.screenTransitionFinish(SCREEN_NAME_CONFIRMATION)
+        }
+
+        identityViewModel.sendAnalyticsRequest(
+            identityViewModel.identityAnalyticsRequestFactory.screenPresented(
+                screenName = SCREEN_NAME_CONFIRMATION
+            )
+        )
+    }
+}
---
+++ link/src/main/java/com/stripe/android/link/LinkActivityResult.kt
@@ -0,0 +1,37 @@
+package com.stripe.android.link
+
+import android.app.Activity
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+sealed class LinkActivityResult(
+    val resultCode: Int
+) : Parcelable {
+    /**
+     * Indicates that the flow was completed successfully and the Stripe Intent was confirmed.
+     */
+    @Parcelize
+    object Completed : LinkActivityResult(Activity.RESULT_OK)
+
+    /**
+     * The user cancelled the Link flow without completing it.
+     */
+    @Parcelize
+    data class Canceled(
+        val reason: Reason
+    ) : LinkActivityResult(Activity.RESULT_CANCELED) {
+        enum class Reason {
+            BackPressed,
+            PayAnotherWay,
+            LoggedOut
+        }
+    }
+
+    /**
+     * Something went wrong. See [error] for more information.
+     */
+    @Parcelize
+    data class Failed(
+        val error: Throwable
+    ) : LinkActivityResult(Activity.RESULT_CANCELED)
+}
