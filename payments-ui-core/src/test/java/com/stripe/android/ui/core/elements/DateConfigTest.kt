541cd6d56283ac95fa58349e54c53f7888000c5f
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 09:26:21 +1000

    Initialize carlosmuvi/BANKCON-7148/fc-networking-supportability-phase-2

---
+++ payments-ui-core/src/test/java/com/stripe/android/ui/core/elements/DateConfigTest.kt
@@ -0,0 +1,233 @@
+package com.stripe.android.ui.core.elements
+
+import com.google.common.truth.Truth
+import com.stripe.android.uicore.elements.DateConfig
+import com.stripe.android.uicore.elements.TextFieldStateConstants
+import org.junit.Test
+import java.util.Calendar
+import com.stripe.android.uicore.R as UiCoreR
+
+class DateConfigTest {
+    private val dateConfig = DateConfig()
+
+    @Test
+    fun `only numbers are allowed in the field`() {
+        Truth.assertThat(dateConfig.filter("123^@Numbe/-r[\uD83E\uDD57."))
+            .isEqualTo("123")
+    }
+
+    @Test
+    fun `blank number returns blank state`() {
+        Truth.assertThat(dateConfig.determineState(""))
+            .isEqualTo(TextFieldStateConstants.Error.Blank)
+    }
+
+    @Test
+    fun `incomplete number is in incomplete state`() {
+        val state = dateConfig.determineState("12")
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Error.Incomplete::class.java)
+        Truth.assertThat(
+            state.getError()?.errorMessage
+        ).isEqualTo(UiCoreR.string.stripe_incomplete_expiry_date)
+    }
+
+    @Test
+    fun `date is too long`() {
+        val state = dateConfig.determineState("1234567890123456789")
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Error.Invalid::class.java)
+        Truth.assertThat(
+            state.getError()?.errorMessage
+        ).isEqualTo(UiCoreR.string.stripe_incomplete_expiry_date)
+    }
+
+    @Test
+    fun `date invalid month and 2 digit year`() {
+        val state = dateConfig.determineState("1955")
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Error.Invalid::class.java)
+        Truth.assertThat(
+            state.getError()?.errorMessage
+        ).isEqualTo(UiCoreR.string.stripe_incomplete_expiry_date)
+    }
+
+    @Test
+    fun `date in the past`() {
+        val state = dateConfig.determineState("1299")
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Error.Invalid::class.java)
+        Truth.assertThat(
+            state.getError()?.errorMessage
+        ).isEqualTo(UiCoreR.string.stripe_invalid_expiry_year)
+    }
+
+    @Test
+    fun `date in the near past`() {
+        val state = dateConfig.determineState("1220")
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Error.Invalid::class.java)
+        Truth.assertThat(
+            state.getError()?.errorMessage
+        ).isEqualTo(UiCoreR.string.stripe_invalid_expiry_year)
+    }
+
+    @Test
+    fun `current month and year`() {
+        val input = produceInput(
+            month = get1BasedCurrentMonth(),
+            year = Calendar.getInstance().get(Calendar.YEAR) % 100,
+        )
+
+        val state = dateConfig.determineState(input)
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Valid.Full::class.java)
+    }
+
+    @Test
+    fun `next month`() {
+        var month = get1BasedCurrentMonth()
+        var year = Calendar.getInstance().get(Calendar.YEAR) % 100
+
+        if (month == 12) {
+            month = 1
+            year += 1
+        }
+
+        val input = produceInput(month, year)
+        val state = dateConfig.determineState(input)
+
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Valid.Full::class.java)
+    }
+
+    @Test
+    fun `current month and year + 1`() {
+        val input = produceInput(
+            month = get1BasedCurrentMonth(),
+            year = (Calendar.getInstance().get(Calendar.YEAR) + 1) % 100,
+        )
+        val state = dateConfig.determineState(input)
+
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Valid.Full::class.java)
+    }
+
+    @Test
+    fun `current month - 1 and year`() {
+        var previousMonth = get1BasedCurrentMonth() - 1
+        var year = Calendar.getInstance().get(Calendar.YEAR) % 100
+        var expectedErrorMessage = UiCoreR.string.stripe_invalid_expiry_month
+
+        // On January, use December of previous year.
+        if (previousMonth == 0) {
+            previousMonth = 12
+            year -= 1
+            expectedErrorMessage = UiCoreR.string.stripe_invalid_expiry_year
+        }
+
+        val input = produceInput(
+            month = previousMonth,
+            year = year,
+        )
+
+        val state = dateConfig.determineState(input)
+
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Error.Invalid::class.java)
+        Truth.assertThat(
+            state.getError()?.errorMessage
+        ).isEqualTo(expectedErrorMessage)
+    }
+
+    @Test
+    fun `current month and year - 1`() {
+        val input = produceInput(
+            month = get1BasedCurrentMonth(),
+            year = (Calendar.getInstance().get(Calendar.YEAR) - 1) % 100,
+        )
+
+        val state = dateConfig.determineState(input)
+
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Error.Invalid::class.java)
+        Truth.assertThat(
+            state.getError()?.errorMessage
+        ).isEqualTo(UiCoreR.string.stripe_invalid_expiry_year)
+    }
+
+    @Test
+    fun `card expire 51 years from now`() {
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        val state = DateConfig.determineTextFieldState(
+            3,
+            (currentYear + 51) % 100,
+            2,
+            currentYear
+        )
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Error.Invalid::class.java)
+        Truth.assertThat(
+            state.getError()?.errorMessage
+        ).isEqualTo(UiCoreR.string.stripe_invalid_expiry_year)
+    }
+
+    @Test
+    fun `card expire 50 years from now`() {
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        val state = DateConfig.determineTextFieldState(
+            3,
+            (currentYear + 50) % 100,
+            2,
+            currentYear
+        )
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Valid.Full::class.java)
+    }
+
+    private fun get1BasedCurrentMonth() = Calendar.getInstance().get(Calendar.MONTH) + 1
+
+    @Test
+    fun `date is valid 2 digit month and 2 digit year`() {
+        val state = dateConfig.determineState("1255")
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Valid.Full::class.java)
+    }
+
+    @Test
+    fun `date is invalid 2X month and 2 digit year`() {
+        val state = dateConfig.determineState("2123")
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Error.Invalid::class.java)
+        Truth.assertThat(
+            state.getError()?.errorMessage
+        ).isEqualTo(UiCoreR.string.stripe_incomplete_expiry_date)
+    }
+
+    @Test
+    fun `date is valid one digit month two digit year`() {
+        val state = dateConfig.determineState("130")
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Valid.Full::class.java)
+    }
+
+    @Test
+    fun `date is valid 0X month two digit year`() {
+        val state = dateConfig.determineState("0130")
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Valid.Full::class.java)
+    }
+
+    @Test
+    fun `date is valid 2X month and 2 digit year`() {
+        val state = dateConfig.determineState("230")
+        Truth.assertThat(state)
+            .isInstanceOf(TextFieldStateConstants.Valid.Full::class.java)
+    }
+
+    private fun produceInput(month: Int, year: Int): String {
+        val formattedMonth = month.toString().padStart(length = 2, padChar = '0')
+        val formattedYear = year.toString().padStart(length = 2, padChar = '0')
+        return formattedMonth + formattedYear
+    }
+}
---
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/analytics/DefaultEventReporter.kt
@@ -0,0 +1,226 @@
+package com.stripe.android.paymentsheet.analytics
+
+import com.stripe.android.core.injection.IOContext
+import com.stripe.android.core.networking.AnalyticsRequestExecutor
+import com.stripe.android.core.utils.DurationProvider
+import com.stripe.android.model.PaymentMethodCode
+import com.stripe.android.networking.PaymentAnalyticsRequestFactory
+import com.stripe.android.paymentsheet.DeferredIntentConfirmationType
+import com.stripe.android.paymentsheet.PaymentSheet
+import com.stripe.android.paymentsheet.model.PaymentSelection
+import com.stripe.android.paymentsheet.state.asPaymentSheetLoadingException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlin.coroutines.CoroutineContext
+
+@Singleton
+internal class DefaultEventReporter @Inject internal constructor(
+    private val mode: EventReporter.Mode,
+    private val analyticsRequestExecutor: AnalyticsRequestExecutor,
+    private val paymentAnalyticsRequestFactory: PaymentAnalyticsRequestFactory,
+    private val durationProvider: DurationProvider,
+    @IOContext private val workContext: CoroutineContext
+) : EventReporter {
+
+    override fun onInit(
+        configuration: PaymentSheet.Configuration?,
+        isDecoupling: Boolean,
+    ) {
+        fireEvent(
+            PaymentSheetEvent.Init(
+                mode = mode,
+                configuration = configuration,
+                isDecoupled = isDecoupling,
+            )
+        )
+    }
+
+    override fun onLoadStarted(isDecoupling: Boolean) {
+        durationProvider.start(DurationProvider.Key.Loading)
+        fireEvent(PaymentSheetEvent.LoadStarted(isDecoupling))
+    }
+
+    override fun onLoadSucceeded(isDecoupling: Boolean) {
+        val duration = durationProvider.end(DurationProvider.Key.Loading)
+        fireEvent(
+            PaymentSheetEvent.LoadSucceeded(
+                duration = duration,
+                isDecoupled = isDecoupling,
+            )
+        )
+    }
+
+    override fun onLoadFailed(
+        isDecoupling: Boolean,
+        error: Throwable,
+    ) {
+        val duration = durationProvider.end(DurationProvider.Key.Loading)
+        fireEvent(
+            PaymentSheetEvent.LoadFailed(
+                duration = duration,
+                error = error.asPaymentSheetLoadingException.type,
+                isDecoupled = isDecoupling,
+            )
+        )
+    }
+
+    override fun onDismiss(
+        isDecoupling: Boolean,
+    ) {
+        fireEvent(
+            PaymentSheetEvent.Dismiss(
+                isDecoupled = isDecoupling,
+            )
+        )
+    }
+
+    override fun onShowExistingPaymentOptions(
+        linkEnabled: Boolean,
+        currency: String?,
+        isDecoupling: Boolean,
+    ) {
+        durationProvider.start(DurationProvider.Key.Checkout)
+
+        fireEvent(
+            PaymentSheetEvent.ShowExistingPaymentOptions(
+                mode = mode,
+                linkEnabled = linkEnabled,
+                currency = currency,
+                isDecoupled = isDecoupling,
+            )
+        )
+    }
+
+    override fun onShowNewPaymentOptionForm(
+        linkEnabled: Boolean,
+        currency: String?,
+        isDecoupling: Boolean,
+    ) {
+        durationProvider.start(DurationProvider.Key.Checkout)
+
+        fireEvent(
+            PaymentSheetEvent.ShowNewPaymentOptionForm(
+                mode = mode,
+                linkEnabled = linkEnabled,
+                currency = currency,
+                isDecoupled = isDecoupling,
+            )
+        )
+    }
+
+    override fun onSelectPaymentMethod(
+        code: PaymentMethodCode,
+        currency: String?,
+        isDecoupling: Boolean,
+    ) {
+        fireEvent(
+            PaymentSheetEvent.SelectPaymentMethod(
+                code = code,
+                isDecoupled = isDecoupling,
+                currency = currency,
+            )
+        )
+    }
+
+    override fun onSelectPaymentOption(
+        paymentSelection: PaymentSelection,
+        currency: String?,
+        isDecoupling: Boolean,
+    ) {
+        fireEvent(
+            PaymentSheetEvent.SelectPaymentOption(
+                mode = mode,
+                paymentSelection = paymentSelection,
+                currency = currency,
+                isDecoupled = isDecoupling,
+            )
+        )
+    }
+
+    override fun onPressConfirmButton(currency: String?, isDecoupling: Boolean) {
+        fireEvent(
+            PaymentSheetEvent.PressConfirmButton(
+                currency = currency,
+                isDecoupled = isDecoupling,
+            )
+        )
+    }
+
+    override fun onPaymentSuccess(
+        paymentSelection: PaymentSelection?,
+        currency: String?,
+        deferredIntentConfirmationType: DeferredIntentConfirmationType?,
+    ) {
+        // Wallets are treated as a saved payment method after confirmation, so we need
+        // to "reset" to the correct PaymentSelection for accurate reporting.
+        val savedSelection = (paymentSelection as? PaymentSelection.Saved)
+
+        val realSelection = savedSelection?.walletType?.paymentSelection ?: paymentSelection
+        val duration = durationProvider.end(DurationProvider.Key.Checkout)
+
+        fireEvent(
+            PaymentSheetEvent.Payment(
+                mode = mode,
+                paymentSelection = realSelection,
+                duration = duration,
+                result = PaymentSheetEvent.Payment.Result.Success,
+                currency = currency,
+                isDecoupled = deferredIntentConfirmationType != null,
+                deferredIntentConfirmationType = deferredIntentConfirmationType,
+            )
+        )
+    }
+
+    override fun onPaymentFailure(
+        paymentSelection: PaymentSelection?,
+        currency: String?,
+        isDecoupling: Boolean,
+    ) {
+        val duration = durationProvider.end(DurationProvider.Key.Checkout)
+
+        fireEvent(
+            PaymentSheetEvent.Payment(
+                mode = mode,
+                paymentSelection = paymentSelection,
+                duration = duration,
+                result = PaymentSheetEvent.Payment.Result.Failure,
+                currency = currency,
+                isDecoupled = isDecoupling,
+                deferredIntentConfirmationType = null,
+            )
+        )
+    }
+
+    override fun onLpmSpecFailure(
+        isDecoupling: Boolean,
+    ) {
+        fireEvent(
+            PaymentSheetEvent.LpmSerializeFailureEvent(isDecoupled = isDecoupling)
+        )
+    }
+
+    override fun onAutofill(
+        type: String,
+        isDecoupling: Boolean,
+    ) {
+        fireEvent(
+            PaymentSheetEvent.AutofillEvent(
+                type = type,
+                isDecoupled = isDecoupling,
+            )
+        )
+    }
+
+    private fun fireEvent(event: PaymentSheetEvent) {
+        CoroutineScope(workContext).launch {
+            analyticsRequestExecutor.executeAsync(
+                paymentAnalyticsRequestFactory.createRequest(
+                    event = event,
+                    additionalParams = event.params,
+                )
+            )
+        }
+    }
+}
