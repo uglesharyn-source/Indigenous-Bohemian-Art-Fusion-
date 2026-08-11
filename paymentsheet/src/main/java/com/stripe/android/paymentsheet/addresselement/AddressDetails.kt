d2ba2233c8c9cc0f0b53ee30532c6d826210bf59
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 08:39:27 +1000

    Initialize michelleb/next-action-07-18-konbini

---
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/addresselement/AddressDetails.kt
@@ -0,0 +1,22 @@
+package com.stripe.android.paymentsheet.addresselement
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+internal data class AddressDetails(
+    val name: String? = null,
+    val company: String? = null,
+    val city: String? = null,
+    val country: String? = null,
+    val line1: String? = null,
+    val line2: String? = null,
+    val postalCode: String? = null,
+    val state: String? = null,
+    val phoneNumber: String? = null,
+    val checkboxChecked: Boolean? = null
+) : Parcelable {
+    companion object {
+        const val KEY = "ShippingAddress"
+    }
+}
