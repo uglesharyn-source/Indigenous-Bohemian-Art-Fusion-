4688f0582ca3665287d5ea0855875da7e8bebafc
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 08:56:08 +1000

    Initialize carlosmuvi/record-bitrise-emulator

---
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/CardDetailsSectionElement.kt
@@ -0,0 +1,24 @@
+package com.stripe.android.ui.core.elements
+
+import android.content.Context
+import androidx.annotation.RestrictTo
+import com.stripe.android.uicore.elements.FormElement
+import com.stripe.android.uicore.elements.IdentifierSpec
+import com.stripe.android.uicore.forms.FormFieldEntry
+import kotlinx.coroutines.flow.Flow
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+class CardDetailsSectionElement(
+    val context: Context,
+    initialValues: Map<IdentifierSpec, String?>,
+    viewOnlyFields: Set<IdentifierSpec>,
+    override val identifier: IdentifierSpec,
+    override val controller: CardDetailsSectionController =
+        CardDetailsSectionController(context, initialValues, viewOnlyFields)
+) : FormElement {
+    override fun getFormFieldValueFlow(): Flow<List<Pair<IdentifierSpec, FormFieldEntry>>> =
+        controller.cardDetailsElement.getFormFieldValueFlow()
+
+    override fun getTextFieldIdentifiers(): Flow<List<IdentifierSpec>> =
+        controller.cardDetailsElement.getTextFieldIdentifiers()
+}
