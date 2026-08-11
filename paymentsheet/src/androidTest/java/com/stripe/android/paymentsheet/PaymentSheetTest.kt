0b9dfefadcc24bc5525209d38fe06597f36a9804
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 09:02:13 +1000

    Initialize jaynewstrom/browserstack

---
+++ paymentsheet/src/androidTest/java/com/stripe/android/paymentsheet/PaymentSheetTest.kt
@@ -0,0 +1,276 @@
+package com.stripe.android.paymentsheet
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.lifecycle.Lifecycle
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import com.stripe.android.CreateIntentResult
+import com.stripe.android.ExperimentalPaymentSheetDecouplingApi
+import com.stripe.android.PaymentConfiguration
+import com.stripe.android.networktesting.NetworkRule
+import com.stripe.android.networktesting.RequestMatchers.method
+import com.stripe.android.networktesting.RequestMatchers.path
+import com.stripe.android.networktesting.testBodyFromFile
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+@RunWith(AndroidJUnit4::class)
+internal class PaymentSheetTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    private val page: PaymentSheetPage = PaymentSheetPage(composeTestRule)
+
+    @get:Rule
+    val networkRule = NetworkRule()
+
+    @Test
+    fun testSuccessfulCardPayment() {
+        networkRule.enqueue(
+            method("GET"),
+            path("/v1/elements/sessions"),
+        ) { response ->
+            response.testBodyFromFile("elements-sessions-requires_payment_method.json")
+        }
+
+        val countDownLatch = CountDownLatch(1)
+        val activityScenarioRule = composeTestRule.activityRule
+        val scenario = activityScenarioRule.scenario
+        scenario.moveToState(Lifecycle.State.CREATED)
+        lateinit var paymentSheet: PaymentSheet
+        scenario.onActivity {
+            PaymentConfiguration.init(it, "pk_test_123")
+            paymentSheet = PaymentSheet(it) { result ->
+                assertThat(result).isInstanceOf(PaymentSheetResult.Completed::class.java)
+                countDownLatch.countDown()
+            }
+        }
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        scenario.onActivity {
+            paymentSheet.presentWithPaymentIntent(
+                paymentIntentClientSecret = "pi_example_secret_example",
+                configuration = null,
+            )
+        }
+
+        page.fillOutCardDetails()
+
+        networkRule.enqueue(
+            method("POST"),
+            path("/v1/payment_intents/pi_example/confirm"),
+        ) { response ->
+            response.testBodyFromFile("payment-intent-confirm.json")
+        }
+
+        page.clickPrimaryButton()
+
+        assertThat(countDownLatch.await(5, TimeUnit.SECONDS)).isTrue()
+    }
+
+    @Test
+    fun testSuccessfulDelayedSuccessPayment() {
+        networkRule.enqueue(
+            method("GET"),
+            path("/v1/elements/sessions"),
+        ) { response ->
+            response.testBodyFromFile("elements-sessions-requires_payment_method.json")
+        }
+
+        val countDownLatch = CountDownLatch(1)
+        val activityScenarioRule = composeTestRule.activityRule
+        val scenario = activityScenarioRule.scenario
+        scenario.moveToState(Lifecycle.State.CREATED)
+        lateinit var paymentSheet: PaymentSheet
+        scenario.onActivity {
+            PaymentConfiguration.init(it, "pk_test_123")
+            paymentSheet = PaymentSheet(it) { result ->
+                assertThat(result).isInstanceOf(PaymentSheetResult.Completed::class.java)
+                countDownLatch.countDown()
+            }
+        }
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        scenario.onActivity {
+            paymentSheet.presentWithPaymentIntent(
+                paymentIntentClientSecret = "pi_example_secret_example",
+                configuration = null,
+            )
+        }
+
+        page.fillOutCardDetails()
+
+        networkRule.enqueue(
+            method("POST"),
+            path("/v1/payment_intents/pi_example/confirm"),
+        ) { response ->
+            response.testBodyFromFile("payment-intent-confirm_with-requires_action-status.json")
+        }
+
+        networkRule.enqueue(
+            method("GET"),
+            path("/v1/payment_intents/pi_example"),
+        ) { response ->
+            response.testBodyFromFile("payment-intent-get-success.json")
+        }
+
+        page.clickPrimaryButton()
+
+        assertThat(countDownLatch.await(5, TimeUnit.SECONDS)).isTrue()
+    }
+
+    @Test
+    fun testFailureWhenSetupRequestsFail() {
+        networkRule.enqueue(
+            method("GET"),
+            path("/v1/elements/sessions"),
+        ) { response ->
+            response.setResponseCode(400)
+        }
+
+        networkRule.enqueue(
+            method("GET"),
+            path("/v1/payment_intents/pi_example"),
+        ) { response ->
+            response.setResponseCode(400)
+        }
+
+        val countDownLatch = CountDownLatch(1)
+        val activityScenarioRule = composeTestRule.activityRule
+        val scenario = activityScenarioRule.scenario
+        scenario.moveToState(Lifecycle.State.CREATED)
+        lateinit var paymentSheet: PaymentSheet
+        scenario.onActivity {
+            PaymentConfiguration.init(it, "pk_test_123")
+            paymentSheet = PaymentSheet(it) { result ->
+                assertThat(result).isInstanceOf(PaymentSheetResult.Failed::class.java)
+                countDownLatch.countDown()
+            }
+        }
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        scenario.onActivity {
+            paymentSheet.presentWithPaymentIntent(
+                paymentIntentClientSecret = "pi_example_secret_example",
+                configuration = null,
+            )
+        }
+
+        assertThat(countDownLatch.await(5, TimeUnit.SECONDS)).isTrue()
+    }
+
+    @OptIn(ExperimentalPaymentSheetDecouplingApi::class)
+    @Test
+    fun testDeferredIntentCardPayment() {
+        networkRule.enqueue(
+            method("GET"),
+            path("/v1/elements/sessions"),
+        ) { response ->
+            response.testBodyFromFile("elements-sessions-deferred_payment_intent.json")
+        }
+
+        val countDownLatch = CountDownLatch(1)
+        val activityScenarioRule = composeTestRule.activityRule
+        val scenario = activityScenarioRule.scenario
+        scenario.moveToState(Lifecycle.State.CREATED)
+        lateinit var paymentSheet: PaymentSheet
+        scenario.onActivity {
+            PaymentConfiguration.init(it, "pk_test_123")
+            paymentSheet = PaymentSheet(
+                activity = it,
+                createIntentCallback = {
+                    CreateIntentResult.Success("pi_example_secret_example")
+                }
+            ) { result ->
+                assertThat(result).isInstanceOf(PaymentSheetResult.Completed::class.java)
+                countDownLatch.countDown()
+            }
+        }
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        scenario.onActivity {
+            paymentSheet.presentWithIntentConfiguration(
+                intentConfiguration = PaymentSheet.IntentConfiguration(
+                    mode = PaymentSheet.IntentConfiguration.Mode.Payment(
+                        amount = 2000,
+                        currency = "usd"
+                    )
+                ),
+                configuration = null,
+            )
+        }
+
+        page.fillOutCardDetails()
+
+        networkRule.enqueue(
+            method("POST"),
+            path("/v1/payment_methods"),
+        ) { response ->
+            response.testBodyFromFile("payment-methods-create.json")
+        }
+
+        networkRule.enqueue(
+            method("POST"),
+            path("/v1/payment_intents/pi_example/confirm"),
+        ) { response ->
+            response.testBodyFromFile("payment-intent-confirm.json")
+        }
+
+        page.clickPrimaryButton()
+
+        assertThat(countDownLatch.await(5, TimeUnit.SECONDS)).isTrue()
+    }
+
+    @OptIn(ExperimentalPaymentSheetDecouplingApi::class)
+    @Test
+    fun testDeferredIntentFailedCardPayment() {
+        networkRule.enqueue(
+            method("GET"),
+            path("/v1/elements/sessions"),
+        ) { response ->
+            response.testBodyFromFile("elements-sessions-deferred_payment_intent.json")
+        }
+
+        val activityScenarioRule = composeTestRule.activityRule
+        val scenario = activityScenarioRule.scenario
+        scenario.moveToState(Lifecycle.State.CREATED)
+        lateinit var paymentSheet: PaymentSheet
+        scenario.onActivity {
+            PaymentConfiguration.init(it, "pk_test_123")
+            paymentSheet = PaymentSheet(
+                activity = it,
+                createIntentCallback = {
+                    CreateIntentResult.Failure(
+                        cause = Exception("We don't accept visa"),
+                        displayMessage = "We don't accept visa"
+                    )
+                }
+            ) {
+                error("Shouldn't call PaymentSheetResultCallback")
+            }
+        }
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        scenario.onActivity {
+            paymentSheet.presentWithIntentConfiguration(
+                intentConfiguration = PaymentSheet.IntentConfiguration(
+                    mode = PaymentSheet.IntentConfiguration.Mode.Payment(
+                        amount = 2000,
+                        currency = "usd"
+                    )
+                ),
+                configuration = null,
+            )
+        }
+
+        page.fillOutCardDetails()
+
+        networkRule.enqueue(
+            method("POST"),
+            path("/v1/payment_methods"),
+        ) { response ->
+            response.testBodyFromFile("payment-methods-create.json")
+        }
+
+        page.clickPrimaryButton()
+        page.waitForText("We don't accept visa")
+    }
+}
---
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/state/PaymentSheetLoader.kt
@@ -0,0 +1,307 @@
+package com.stripe.android.paymentsheet.state
+
+import com.stripe.android.core.Logger
+import com.stripe.android.core.injection.IOContext
+import com.stripe.android.googlepaylauncher.GooglePayEnvironment
+import com.stripe.android.googlepaylauncher.GooglePayRepository
+import com.stripe.android.link.LinkPaymentLauncher
+import com.stripe.android.link.LinkPaymentLauncher.Companion.supportedFundingSources
+import com.stripe.android.link.model.AccountStatus
+import com.stripe.android.model.PaymentMethod
+import com.stripe.android.model.PaymentMethod.Type.Link
+import com.stripe.android.model.StripeIntent
+import com.stripe.android.payments.core.injection.APP_NAME
+import com.stripe.android.paymentsheet.PaymentSheet
+import com.stripe.android.paymentsheet.PrefsRepository
+import com.stripe.android.paymentsheet.addresselement.AddressDetails
+import com.stripe.android.paymentsheet.addresselement.toIdentifierMap
+import com.stripe.android.paymentsheet.analytics.EventReporter
+import com.stripe.android.paymentsheet.model.PaymentSelection
+import com.stripe.android.paymentsheet.model.SavedSelection
+import com.stripe.android.paymentsheet.model.StripeIntentValidator
+import com.stripe.android.paymentsheet.model.getSupportedSavedCustomerPMs
+import com.stripe.android.paymentsheet.repositories.CustomerRepository
+import com.stripe.android.paymentsheet.repositories.ElementsSessionRepository
+import com.stripe.android.ui.core.BillingDetailsCollectionConfiguration
+import com.stripe.android.ui.core.forms.resources.LpmRepository
+import com.stripe.android.ui.core.forms.resources.LpmRepository.ServerSpecState
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+import javax.inject.Named
+import javax.inject.Singleton
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * Loads the information necessary to display [PaymentSheet], either directly or via
+ * [PaymentSheet.FlowController].
+ */
+internal interface PaymentSheetLoader {
+
+    suspend fun load(
+        initializationMode: PaymentSheet.InitializationMode,
+        paymentSheetConfiguration: PaymentSheet.Configuration? = null
+    ): Result
+
+    sealed class Result {
+        data class Success(val state: PaymentSheetState.Full) : Result()
+        class Failure(val throwable: Throwable) : Result()
+    }
+}
+
+@Singleton
+internal class DefaultPaymentSheetLoader @Inject constructor(
+    @Named(APP_NAME) private val appName: String,
+    private val prefsRepositoryFactory: @JvmSuppressWildcards (PaymentSheet.CustomerConfiguration?) -> PrefsRepository,
+    private val googlePayRepositoryFactory: @JvmSuppressWildcards (GooglePayEnvironment) -> GooglePayRepository,
+    private val elementsSessionRepository: ElementsSessionRepository,
+    private val stripeIntentValidator: StripeIntentValidator,
+    private val customerRepository: CustomerRepository,
+    private val lpmRepository: LpmRepository,
+    private val logger: Logger,
+    private val eventReporter: EventReporter,
+    @IOContext private val workContext: CoroutineContext,
+    private val accountStatusProvider: LinkAccountStatusProvider,
+) : PaymentSheetLoader {
+
+    override suspend fun load(
+        initializationMode: PaymentSheet.InitializationMode,
+        paymentSheetConfiguration: PaymentSheet.Configuration?
+    ): PaymentSheetLoader.Result = withContext(workContext) {
+        val isGooglePayReady = isGooglePayReady(paymentSheetConfiguration)
+
+        runCatching {
+            retrieveElementsSession(
+                initializationMode = initializationMode,
+                configuration = paymentSheetConfiguration,
+            )
+        }.fold(
+            onSuccess = { stripeIntent ->
+                create(
+                    stripeIntent = stripeIntent,
+                    customerConfig = paymentSheetConfiguration?.customer,
+                    config = paymentSheetConfiguration,
+                    isGooglePayReady = isGooglePayReady,
+                )
+            },
+            onFailure = {
+                logger.error("Failure initializing FlowController", it)
+                PaymentSheetLoader.Result.Failure(it)
+            }
+        )
+    }
+
+    private suspend fun isGooglePayReady(
+        paymentSheetConfiguration: PaymentSheet.Configuration?
+    ): Boolean {
+        return paymentSheetConfiguration?.googlePay?.environment?.let { environment ->
+            googlePayRepositoryFactory(
+                when (environment) {
+                    PaymentSheet.GooglePayConfiguration.Environment.Production ->
+                        GooglePayEnvironment.Production
+                    PaymentSheet.GooglePayConfiguration.Environment.Test ->
+                        GooglePayEnvironment.Test
+                }
+            )
+        }?.isReady()?.first() ?: false
+    }
+
+    private suspend fun create(
+        stripeIntent: StripeIntent,
+        customerConfig: PaymentSheet.CustomerConfiguration?,
+        config: PaymentSheet.Configuration?,
+        isGooglePayReady: Boolean,
+    ): PaymentSheetLoader.Result = coroutineScope {
+        val prefsRepository = prefsRepositoryFactory(customerConfig)
+
+        val isLinkAvailable = stripeIntent.paymentMethodTypes.contains(Link.code) &&
+            stripeIntent.linkFundingSources.intersect(supportedFundingSources).isNotEmpty()
+
+        val savedSelection = async {
+            prefsRepository.getSavedSelection(
+                isGooglePayAvailable = isGooglePayReady,
+                isLinkAvailable = isLinkAvailable,
+            )
+        }
+
+        val paymentMethods = async {
+            if (customerConfig != null) {
+                retrieveCustomerPaymentMethods(
+                    stripeIntent = stripeIntent,
+                    config = config,
+                    customerConfig = customerConfig,
+                )
+            } else {
+                emptyList()
+            }
+        }
+
+        val sortedPaymentMethods = async {
+            paymentMethods.await().withLastUsedPaymentMethodFirst(
+                savedSelection = savedSelection.await(),
+            )
+        }
+
+        val initialPaymentSelection = async {
+            val desiredSelection = when (val selection = savedSelection.await()) {
+                is SavedSelection.GooglePay -> {
+                    PaymentSelection.GooglePay
+                }
+                is SavedSelection.Link -> {
+                    PaymentSelection.Link
+                }
+                is SavedSelection.PaymentMethod -> {
+                    paymentMethods.await().find { it.id == selection.id }?.toPaymentSelection()
+                }
+                is SavedSelection.None -> {
+                    null
+                }
+            }
+
+            desiredSelection ?: sortedPaymentMethods.await().firstOrNull()?.toPaymentSelection()
+        }
+
+        val linkState = async {
+            if (isLinkAvailable) {
+                loadLinkState(config, stripeIntent)
+            } else {
+                null
+            }
+        }
+
+        return@coroutineScope PaymentSheetLoader.Result.Success(
+            PaymentSheetState.Full(
+                config = config,
+                stripeIntent = stripeIntent,
+                customerPaymentMethods = sortedPaymentMethods.await(),
+                isGooglePayReady = isGooglePayReady,
+                linkState = linkState.await(),
+                paymentSelection = initialPaymentSelection.await(),
+            )
+        )
+    }
+
+    private suspend fun retrieveCustomerPaymentMethods(
+        stripeIntent: StripeIntent,
+        config: PaymentSheet.Configuration?,
+        customerConfig: PaymentSheet.CustomerConfiguration,
+    ): List<PaymentMethod> {
+        val paymentMethodTypes = getSupportedSavedCustomerPMs(
+            stripeIntent,
+            config,
+            lpmRepository,
+        ).mapNotNull {
+            // The SDK is only able to parse customer LPMs
+            // that are hard coded in the SDK.
+            PaymentMethod.Type.fromCode(it.code)
+        }
+
+        return customerRepository.getPaymentMethods(
+            customerConfig = customerConfig,
+            types = paymentMethodTypes,
+        ).filter { paymentMethod ->
+            paymentMethod.hasExpectedDetails() &&
+                // PayPal isn't supported yet as a saved payment method (backend limitation).
+                paymentMethod.type != PaymentMethod.Type.PayPal
+        }
+    }
+
+    private suspend fun retrieveElementsSession(
+        initializationMode: PaymentSheet.InitializationMode,
+        configuration: PaymentSheet.Configuration?,
+    ): StripeIntent {
+        val elementsSession = elementsSessionRepository.get(initializationMode)
+
+        lpmRepository.update(
+            stripeIntent = elementsSession.stripeIntent,
+            serverLpmSpecs = elementsSession.paymentMethodSpecs,
+            billingDetailsCollectionConfiguration =
+            configuration?.billingDetailsCollectionConfiguration ?: BillingDetailsCollectionConfiguration(),
+        )
+
+        if (lpmRepository.serverSpecLoadingState is ServerSpecState.ServerNotParsed) {
+            eventReporter.onLpmSpecFailure()
+        }
+
+        return stripeIntentValidator.requireValid(elementsSession.stripeIntent)
+    }
+
+    private suspend fun loadLinkState(
+        config: PaymentSheet.Configuration?,
+        stripeIntent: StripeIntent,
+    ): LinkState {
+        val linkConfig = createLinkConfiguration(config, stripeIntent)
+
+        val loginState = when (accountStatusProvider(linkConfig)) {
+            AccountStatus.Verified -> LinkState.LoginState.LoggedIn
+            AccountStatus.NeedsVerification,
+            AccountStatus.VerificationStarted -> LinkState.LoginState.NeedsVerification
+            AccountStatus.SignedOut,
+            AccountStatus.Error -> LinkState.LoginState.LoggedOut
+        }
+
+        return LinkState(
+            configuration = linkConfig,
+            loginState = loginState,
+        )
+    }
+
+    private suspend fun createLinkConfiguration(
+        config: PaymentSheet.Configuration?,
+        stripeIntent: StripeIntent,
+    ): LinkPaymentLauncher.Configuration {
+        val shippingDetails: AddressDetails? = config?.shippingDetails
+
+        val customerPhone = if (shippingDetails?.isCheckboxSelected == true) {
+            shippingDetails.phoneNumber
+        } else {
+            config?.defaultBillingDetails?.phone
+        }
+
+        val shippingAddress = if (shippingDetails?.isCheckboxSelected == true) {
+            shippingDetails.toIdentifierMap(config.defaultBillingDetails)
+        } else {
+            null
+        }
+
+        val customerEmail = config?.defaultBillingDetails?.email ?: config?.customer?.let {
+            customerRepository.retrieveCustomer(
+                it.id,
+                it.ephemeralKeySecret
+            )
+        }?.email
+
+        val merchantName = config?.merchantDisplayName ?: appName
+
+        return LinkPaymentLauncher.Configuration(
+            stripeIntent = stripeIntent,
+            merchantName = merchantName,
+            customerEmail = customerEmail,
+            customerPhone = customerPhone,
+            customerName = config?.defaultBillingDetails?.name,
+            customerBillingCountryCode = config?.defaultBillingDetails?.address?.country,
+            shippingValues = shippingAddress
+        )
+    }
+}
+
+private fun List<PaymentMethod>.withLastUsedPaymentMethodFirst(
+    savedSelection: SavedSelection,
+): List<PaymentMethod> {
+    val defaultPaymentMethodIndex = (savedSelection as? SavedSelection.PaymentMethod)?.let {
+        indexOfFirst { it.id == savedSelection.id }.takeIf { it != -1 }
+    }
+
+    return if (defaultPaymentMethodIndex != null) {
+        val primaryPaymentMethod = get(defaultPaymentMethodIndex)
+        listOf(primaryPaymentMethod) + (this - primaryPaymentMethod)
+    } else {
+        this
+    }
+}
+
+private fun PaymentMethod.toPaymentSelection(): PaymentSelection.Saved {
+    return PaymentSelection.Saved(this, isGooglePay = false)
+}
