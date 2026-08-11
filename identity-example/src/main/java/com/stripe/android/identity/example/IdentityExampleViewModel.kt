d1ab33de6156fd4c4778e938f50d5f971e037696
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 09:03:22 +1000

    Initialize jameswoo/maestro-test-1

---
+++ identity-example/src/main/java/com/stripe/android/identity/example/IdentityExampleViewModel.kt
@@ -0,0 +1,85 @@
+package com.stripe.android.identity.example
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.liveData
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.coroutines.awaitStringResult
+import com.github.kittinunf.result.Result
+import com.stripe.android.identity.example.ui.VerificationType
+import kotlinx.serialization.json.Json
+
+internal class IdentityExampleViewModel(application: Application) : AndroidViewModel(application) {
+    private val json by lazy {
+        Json {
+            ignoreUnknownKeys = true
+            isLenient = true
+            encodeDefaults = true
+            explicitNulls = false
+        }
+    }
+
+    fun postForResult(
+        type: VerificationType,
+        allowDrivingLicense: Boolean,
+        allowPassport: Boolean,
+        allowId: Boolean,
+        requireLiveCapture: Boolean,
+        requireId: Boolean,
+        requireSelfie: Boolean,
+        requireAddress: Boolean
+    ) = liveData {
+        val result = Fuel.post(EXAMPLE_BACKEND_URL)
+            .header("content-type", "application/json")
+            .body(
+                json.encodeToString(
+                    VerificationSessionCreationRequest.serializer(),
+                    VerificationSessionCreationRequest(
+                        type = type.value,
+                        options =
+                        if (type == VerificationType.DOCUMENT) {
+                            VerificationSessionCreationRequest.Options(
+                                document = VerificationSessionCreationRequest.Document(
+                                    requireIdNumber = requireId,
+                                    requireMatchingSelfie = requireSelfie,
+                                    requireLiveCapture = requireLiveCapture,
+                                    requireAddress = requireAddress,
+                                    allowedTypes = mutableListOf<String>().also {
+                                        if (allowDrivingLicense) it.add(DRIVING_LICENSE)
+                                        if (allowPassport) it.add(PASSPORT)
+                                        if (allowId) it.add(ID_CARD)
+                                    }
+                                )
+                            )
+                        } else {
+                            null
+                        }
+                    )
+                )
+            ).awaitStringResult()
+
+        if (result is Result.Failure) {
+            emit(result)
+        } else {
+            try {
+                json.decodeFromString(
+                    VerificationSessionCreationResponse.serializer(),
+                    result.get()
+                ).let {
+                    emit(Result.success(it))
+                }
+            } catch (t: Throwable) {
+                emit(Result.error(Exception(t)))
+            }
+        }
+    }
+
+    private companion object {
+        const val DRIVING_LICENSE = "driving_license"
+        const val PASSPORT = "passport"
+        const val ID_CARD = "id_card"
+
+        const val EXAMPLE_BACKEND_URL =
+            "https://stripe-mobile-identity-verification-playground.glitch.me/create-verification-session"
+    }
+}
---
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/PaymentOptionsActivity.kt
@@ -0,0 +1,79 @@
+package com.stripe.android.paymentsheet
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.annotation.VisibleForTesting
+import androidx.lifecycle.ViewModelProvider
+import com.stripe.android.paymentsheet.databinding.ActivityPaymentOptionsBinding
+import com.stripe.android.paymentsheet.ui.BaseSheetActivity
+import com.stripe.android.paymentsheet.ui.PaymentOptionsScreen
+import com.stripe.android.paymentsheet.utils.launchAndCollectIn
+import com.stripe.android.uicore.StripeTheme
+
+/**
+ * An `Activity` for selecting a payment option.
+ */
+internal class PaymentOptionsActivity : BaseSheetActivity<PaymentOptionResult>() {
+    @VisibleForTesting
+    internal val viewBinding by lazy {
+        ActivityPaymentOptionsBinding.inflate(layoutInflater)
+    }
+
+    @VisibleForTesting
+    internal var viewModelFactory: ViewModelProvider.Factory = PaymentOptionsViewModel.Factory {
+        requireNotNull(starterArgs)
+    }
+
+    override val viewModel: PaymentOptionsViewModel by viewModels { viewModelFactory }
+
+    private val starterArgs: PaymentOptionContract.Args? by lazy {
+        PaymentOptionContract.Args.fromIntent(intent)
+    }
+
+    override val rootView: ViewGroup by lazy { viewBinding.root }
+    override val bottomSheet: ViewGroup by lazy { viewBinding.bottomSheet }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val starterArgs = initializeStarterArgs()
+        super.onCreate(savedInstanceState)
+
+        if (starterArgs == null) {
+            finish()
+            return
+        }
+
+        starterArgs.statusBarColor?.let {
+            window.statusBarColor = it
+        }
+        setContentView(viewBinding.root)
+
+        viewModel.paymentOptionResult.launchAndCollectIn(this) {
+            closeSheet(it)
+        }
+
+        viewBinding.content.setContent {
+            StripeTheme {
+                PaymentOptionsScreen(viewModel)
+            }
+        }
+    }
+
+    private fun initializeStarterArgs(): PaymentOptionContract.Args? {
+        starterArgs?.state?.config?.appearance?.parseAppearance()
+        earlyExitDueToIllegalState = starterArgs == null
+        return starterArgs
+    }
+
+    override fun setActivityResult(result: PaymentOptionResult) {
+        setResult(
+            result.resultCode,
+            Intent().putExtras(result.toBundle()),
+        )
+    }
+
+    internal companion object {
+        internal const val EXTRA_STARTER_ARGS = BaseSheetActivity.EXTRA_STARTER_ARGS
+    }
+}
