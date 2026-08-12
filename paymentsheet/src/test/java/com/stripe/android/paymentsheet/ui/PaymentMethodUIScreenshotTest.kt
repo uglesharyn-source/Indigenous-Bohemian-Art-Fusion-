5c71b00c9e6e0b32c1775372b5b301c267d7297f
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 00:47:08 +0000

    Initialize samer/test-teardown-playground-state

---
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/ui/PaymentMethodUIScreenshotTest.kt
@@ -0,0 +1,205 @@
+package com.stripe.android.paymentsheet.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.stripe.android.screenshottesting.FontSize
+import com.stripe.android.screenshottesting.PaparazziRule
+import com.stripe.android.screenshottesting.SystemAppearance
+import com.stripe.android.ui.core.R
+import com.stripe.android.utils.screenshots.PaymentSheetAppearance
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+internal class PaymentMethodUIScreenshotTest {
+    @get:Rule
+    val paparazziRule = PaparazziRule(
+        SystemAppearance.entries,
+        PaymentSheetAppearance.entries,
+        FontSize.entries,
+        boxModifier = Modifier
+            .padding(16.dp)
+    )
+
+    @Test
+    fun testSelectedStateForTabMode() {
+        paparazziRule.snapshot {
+            NewPaymentMethodTab(
+                minViewWidth = 100.dp,
+                iconRes = R.drawable.stripe_ic_paymentsheet_pm_card,
+                iconUrl = null,
+                imageLoader = mock(),
+                title = "Card",
+                isSelected = true,
+                isEnabled = true,
+                iconRequiresTinting = true,
+                modifier = Modifier,
+                onItemSelectedListener = {},
+            )
+        }
+    }
+
+    @Test
+    fun testUnselectedStateForTabMode() {
+        paparazziRule.snapshot {
+            NewPaymentMethodTab(
+                minViewWidth = 100.dp,
+                iconRes = R.drawable.stripe_ic_paymentsheet_pm_card,
+                iconUrl = null,
+                imageLoader = mock(),
+                title = "Card",
+                isSelected = false,
+                isEnabled = true,
+                iconRequiresTinting = true,
+                modifier = Modifier,
+                onItemSelectedListener = {},
+            )
+        }
+    }
+
+    @Test
+    fun testDisabledForTabMode() {
+        paparazziRule.snapshot {
+            NewPaymentMethodTab(
+                minViewWidth = 100.dp,
+                iconRes = R.drawable.stripe_ic_paymentsheet_pm_card,
+                iconUrl = null,
+                imageLoader = mock(),
+                title = "Card",
+                isSelected = false,
+                isEnabled = false,
+                iconRequiresTinting = true,
+                modifier = Modifier,
+                onItemSelectedListener = {},
+            )
+        }
+    }
+
+    @Test
+    fun testLongTitleForTabMode() {
+        paparazziRule.snapshot {
+            NewPaymentMethodTab(
+                minViewWidth = 100.dp,
+                iconRes = R.drawable.stripe_ic_paymentsheet_pm_bank,
+                iconUrl = null,
+                imageLoader = mock(),
+                title = "The Greatest US Bank Account",
+                isSelected = false,
+                isEnabled = false,
+                iconRequiresTinting = true,
+                modifier = Modifier,
+                onItemSelectedListener = {},
+            )
+        }
+    }
+
+    @Test
+    fun testSelectedStateForVerticalMode() {
+        paparazziRule.snapshot {
+            NewPaymentMethodRowButton(
+                isEnabled = true,
+                isSelected = true,
+                iconRes = R.drawable.stripe_ic_paymentsheet_pm_card,
+                iconUrl = null,
+                imageLoader = mock(),
+                title = "Card",
+                subtitle = null,
+                iconRequiresTinting = true,
+                onClick = {},
+                modifier = Modifier,
+            )
+        }
+    }
+
+    @Test
+    fun testUnselectedStateForVerticalMode() {
+        paparazziRule.snapshot {
+            NewPaymentMethodRowButton(
+                isEnabled = true,
+                isSelected = false,
+                iconRes = R.drawable.stripe_ic_paymentsheet_pm_card,
+                iconUrl = null,
+                imageLoader = mock(),
+                title = "Card",
+                subtitle = null,
+                iconRequiresTinting = true,
+                onClick = {},
+                modifier = Modifier,
+            )
+        }
+    }
+
+    @Test
+    fun testDisabledForVerticalMode() {
+        paparazziRule.snapshot {
+            NewPaymentMethodRowButton(
+                isEnabled = false,
+                isSelected = false,
+                iconRes = R.drawable.stripe_ic_paymentsheet_pm_card,
+                iconUrl = null,
+                imageLoader = mock(),
+                title = "Card",
+                subtitle = null,
+                iconRequiresTinting = true,
+                onClick = {},
+                modifier = Modifier,
+            )
+        }
+    }
+
+    @Test
+    fun testLongTitleForVerticalMode() {
+        paparazziRule.snapshot {
+            NewPaymentMethodRowButton(
+                isEnabled = true,
+                isSelected = false,
+                iconRes = R.drawable.stripe_ic_paymentsheet_pm_bank,
+                iconUrl = null,
+                imageLoader = mock(),
+                title = "The Greatest US Bank Account",
+                subtitle = null,
+                iconRequiresTinting = true,
+                onClick = {},
+                modifier = Modifier,
+            )
+        }
+    }
+
+    @Test
+    fun testSubtitleForVerticalMode() {
+        paparazziRule.snapshot {
+            NewPaymentMethodRowButton(
+                isEnabled = true,
+                isSelected = false,
+                iconRes = R.drawable.stripe_ic_paymentsheet_pm_klarna,
+                iconUrl = null,
+                imageLoader = mock(),
+                title = "Klarna",
+                subtitle = "Buy now or pay later with Klarna.",
+                iconRequiresTinting = false,
+                onClick = {},
+                modifier = Modifier,
+            )
+        }
+    }
+
+    @Test
+    fun testLongSubtitleForVerticalMode() {
+        paparazziRule.snapshot {
+            NewPaymentMethodRowButton(
+                isEnabled = true,
+                isSelected = false,
+                iconRes = R.drawable.stripe_ic_paymentsheet_pm_klarna,
+                iconUrl = null,
+                imageLoader = mock(),
+                title = "Klarna",
+                subtitle = "A very long subtitle, that you should read all of and you should Buy now or pay later" +
+                    " with Klarna.",
+                iconRequiresTinting = false,
+                onClick = {},
+                modifier = Modifier,
+            )
+        }
+    }
+}
