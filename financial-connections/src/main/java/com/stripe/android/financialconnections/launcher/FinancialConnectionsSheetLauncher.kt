cad032624a83c35b5d1ee6cf2f0c03494f5e0d21
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 01:21:17 +0000

    Initialize jaynewstrom/compose-1.6.8

---
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/launcher/FinancialConnectionsSheetLauncher.kt
@@ -0,0 +1,9 @@
+package com.stripe.android.financialconnections.launcher
+
+import androidx.annotation.RestrictTo
+import com.stripe.android.financialconnections.FinancialConnectionsSheet
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+interface FinancialConnectionsSheetLauncher {
+    fun present(configuration: FinancialConnectionsSheet.Configuration)
+}
