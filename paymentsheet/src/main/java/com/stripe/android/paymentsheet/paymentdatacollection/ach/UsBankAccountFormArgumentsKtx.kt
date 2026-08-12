c91aea87d61d46d56234e8049a96b1df3be3e31c
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 01:39:48 +0000

    Initialize bi-dep-removal-size

---
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/paymentdatacollection/ach/UsBankAccountFormArgumentsKtx.kt
@@ -0,0 +1,53 @@
+package com.stripe.android.paymentsheet.paymentdatacollection.ach
+
+import com.stripe.android.core.strings.ResolvableString
+import com.stripe.android.paymentsheet.paymentdatacollection.ach.USBankAccountFormScreenState.BillingDetailsCollection
+import com.stripe.android.paymentsheet.ui.PrimaryButton
+
+internal fun USBankAccountFormArguments.handleScreenStateChanged(
+    screenState: USBankAccountFormScreenState,
+    enabled: Boolean,
+    onPrimaryButtonClick: () -> Unit,
+) {
+    screenState.error?.let {
+        onError(it)
+    }
+
+    if (screenState is BillingDetailsCollection) {
+        updatePrimaryButton(
+            text = screenState.primaryButtonText,
+            onClick = onPrimaryButtonClick,
+            enabled = enabled,
+            shouldShowProcessingWhenClicked = isCompleteFlow,
+        )
+    } else {
+        // Clear the primary button
+        onUpdatePrimaryButtonUIState { null }
+    }
+
+    onMandateTextChanged(screenState.mandateText, false)
+}
+
+private fun USBankAccountFormArguments.updatePrimaryButton(
+    text: ResolvableString,
+    onClick: () -> Unit,
+    shouldShowProcessingWhenClicked: Boolean,
+    enabled: Boolean,
+) {
+    onUpdatePrimaryButtonUIState {
+        PrimaryButton.UIState(
+            label = text,
+            onClick = {
+                if (shouldShowProcessingWhenClicked) {
+                    onUpdatePrimaryButtonState(PrimaryButton.State.StartProcessing)
+                }
+                onClick()
+                onUpdatePrimaryButtonUIState { button ->
+                    button?.copy(enabled = false)
+                }
+            },
+            enabled = enabled,
+            lockVisible = isCompleteFlow,
+        )
+    }
+}
