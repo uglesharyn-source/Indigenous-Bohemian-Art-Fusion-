99455447487435da9c36d6ed201e3718f16c4056
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 00:56:10 +0000

    Initialize tyler/run-3176-variant

---
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/CardNumberElement.kt
@@ -0,0 +1,15 @@
+package com.stripe.android.ui.core.elements
+
+import com.stripe.android.uicore.elements.IdentifierSpec
+import com.stripe.android.uicore.elements.SectionSingleFieldElement
+
+internal data class CardNumberElement(
+    val _identifier: IdentifierSpec,
+    override val controller: CardNumberController
+) : SectionSingleFieldElement(_identifier) {
+    override val allowsUserInteraction: Boolean = true
+
+    override fun setRawValue(rawValuesMap: Map<IdentifierSpec, String?>) {
+        // Nothing from FormArguments to populate
+    }
+}
