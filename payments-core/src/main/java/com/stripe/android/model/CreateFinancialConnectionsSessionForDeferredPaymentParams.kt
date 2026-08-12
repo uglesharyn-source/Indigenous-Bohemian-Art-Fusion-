ab502266bd81489c4bbd248ad497a0171b007e05
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 01:23:14 +0000

    Initialize focus-on-saved-selection-to-start

---
+++ payments-core/src/main/java/com/stripe/android/model/CreateFinancialConnectionsSessionForDeferredPaymentParams.kt
@@ -0,0 +1,56 @@
+package com.stripe.android.model
+
+import androidx.annotation.RestrictTo
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+data class CreateFinancialConnectionsSessionForDeferredPaymentParams(
+    val uniqueId: String,
+    val initialInstitution: String?,
+    val manualEntryOnly: Boolean?,
+    val searchSession: String?,
+    val verificationMethod: VerificationMethodParam?,
+    val hostedSurface: String?,
+    val customer: String?,
+    val onBehalfOf: String?,
+
+    // PaymentIntent only params
+    val amount: Int?,
+    val currency: String?,
+) {
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            PARAM_UNIQUE_ID to uniqueId,
+            PARAM_INITIAL_INSTITUTION to initialInstitution,
+            PARAM_MANUAL_ENTRY_ONLY to manualEntryOnly,
+            PARAM_SEARCH_SESSION to searchSession,
+            PARAM_VERIFICATION_METHOD to verificationMethod?.value,
+            PARAM_CUSTOMER to customer,
+            PARAM_ON_BEHALF_OF to onBehalfOf,
+            PARAM_HOSTED_SURFACE to hostedSurface,
+            PARAM_AMOUNT to amount,
+            PARAM_CURRENCY to currency
+        )
+    }
+
+    private companion object {
+        const val PARAM_UNIQUE_ID = "unique_id"
+        const val PARAM_INITIAL_INSTITUTION = "initial_institution"
+        const val PARAM_MANUAL_ENTRY_ONLY = "manual_entry_only"
+        const val PARAM_SEARCH_SESSION = "search_session"
+        const val PARAM_HOSTED_SURFACE = "hosted_surface"
+        const val PARAM_VERIFICATION_METHOD = "verification_method"
+        const val PARAM_CUSTOMER = "customer"
+        const val PARAM_ON_BEHALF_OF = "on_behalf_of"
+        const val PARAM_AMOUNT = "amount"
+        const val PARAM_CURRENCY = "currency"
+    }
+}
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+enum class VerificationMethodParam(val value: String) {
+    Automatic("automatic"),
+    Skip("skip"),
+    Microdeposits("microdeposits"),
+    Instant("instant"),
+    InstantOrSkip("instant_or_skip")
+}
