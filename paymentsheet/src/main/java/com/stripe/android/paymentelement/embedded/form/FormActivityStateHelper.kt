f39087123dec8080df8fcb756f2631b278d528ee
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 02:11:49 +0000

    Initialize tolu/link/fix_selected_flicker

---
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/form/FormActivityStateHelper.kt
@@ -0,0 +1,79 @@
+package com.stripe.android.paymentelement.embedded.form
+
+import com.stripe.android.core.strings.ResolvableString
+import com.stripe.android.lpmfoundations.paymentmethod.PaymentMethodMetadata
+import com.stripe.android.model.PaymentIntent
+import com.stripe.android.model.StripeIntent
+import com.stripe.android.paymentelement.EmbeddedPaymentElement
+import com.stripe.android.paymentelement.ExperimentalEmbeddedPaymentElementApi
+import com.stripe.android.paymentelement.embedded.EmbeddedSelectionHolder
+import com.stripe.android.paymentsheet.model.amount
+import com.stripe.android.paymentsheet.model.currency
+import com.stripe.android.paymentsheet.ui.PrimaryButtonProcessingState
+import com.stripe.android.paymentsheet.utils.buyButtonLabel
+import com.stripe.android.ui.core.Amount
+import com.stripe.android.uicore.utils.combineAsStateFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+internal interface FormActivityStateHelper {
+    val state: StateFlow<State>
+
+    data class State(
+        val primaryButtonLabel: ResolvableString,
+        val isEnabled: Boolean,
+        val processingState: PrimaryButtonProcessingState,
+        val isProcessing: Boolean,
+    )
+}
+
+@OptIn(ExperimentalEmbeddedPaymentElementApi::class)
+@Singleton
+internal class DefaultFormActivityStateHelper @Inject constructor(
+    paymentMethodMetadata: PaymentMethodMetadata,
+    selectionHolder: EmbeddedSelectionHolder,
+    configuration: EmbeddedPaymentElement.Configuration,
+) : FormActivityStateHelper {
+    private val primaryButtonProcessingState: MutableStateFlow<PrimaryButtonProcessingState> =
+        MutableStateFlow(PrimaryButtonProcessingState.Idle(null))
+    private val isProcessing = MutableStateFlow(false)
+    private val isEnabled: StateFlow<Boolean> = combineAsStateFlow(
+        selectionHolder.selection,
+        isProcessing
+    ) { selection, processing ->
+        selection != null && !processing
+    }
+    private val primaryButtonLabel = MutableStateFlow(
+        primaryButtonLabel(paymentMethodMetadata.stripeIntent, configuration)
+    )
+
+    override val state: StateFlow<FormActivityStateHelper.State> = combineAsStateFlow(
+        primaryButtonProcessingState,
+        isEnabled,
+        isProcessing,
+        primaryButtonLabel
+    ) { buttonProcessingState, enabled, processing, buttonLabel ->
+        FormActivityStateHelper.State(
+            primaryButtonLabel = buttonLabel,
+            isEnabled = enabled,
+            processingState = buttonProcessingState,
+            isProcessing = processing,
+        )
+    }
+
+    private fun primaryButtonLabel(
+        stripeIntent: StripeIntent,
+        configuration: EmbeddedPaymentElement.Configuration
+    ): ResolvableString {
+        val amount = amount(stripeIntent.amount, stripeIntent.currency)
+        val label = configuration.primaryButtonLabel
+        val isForPaymentIntent = stripeIntent is PaymentIntent
+        return buyButtonLabel(amount, label, isForPaymentIntent)
+    }
+
+    private fun amount(amount: Long?, currency: String?): Amount? {
+        return if (amount != null && currency != null) Amount(amount, currency) else null
+    }
+}
