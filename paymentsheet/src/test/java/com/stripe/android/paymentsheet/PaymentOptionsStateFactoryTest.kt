0a9493e31e7ee24f2b102793711a573c47305a48
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 01:51:29 +0000

    Initialize carlosmuvi/attestation-adds-retry-logic-on-retriable-errors

---
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/PaymentOptionsStateFactoryTest.kt
@@ -0,0 +1,220 @@
+package com.stripe.android.paymentsheet
+
+import com.google.common.truth.Truth.assertThat
+import com.stripe.android.core.strings.resolvableString
+import com.stripe.android.core.utils.FeatureFlags
+import com.stripe.android.model.PaymentMethod
+import com.stripe.android.model.PaymentMethodFixtures
+import com.stripe.android.paymentsheet.model.PaymentSelection
+import com.stripe.android.testing.FeatureFlagTestRule
+import org.junit.Rule
+import org.junit.Test
+
+class PaymentOptionsStateFactoryTest {
+
+    @get:Rule
+    val featureFlagTestRule = FeatureFlagTestRule(
+        featureFlag = FeatureFlags.useNewUpdateCardScreen,
+        isEnabled = false
+    )
+
+    @Test
+    fun `Returns current selection if available`() {
+        val paymentMethods = PaymentMethodFixtures.createCards(3)
+        val paymentMethod = paymentMethods.random()
+
+        val state = PaymentOptionsStateFactory.create(
+            paymentMethods = paymentMethods,
+            showGooglePay = true,
+            showLink = true,
+            currentSelection = PaymentSelection.Saved(paymentMethod),
+            nameProvider = { it!!.resolvableString },
+            isCbcEligible = false,
+            canRemovePaymentMethods = true,
+            defaultPaymentMethodId = null
+        )
+
+        val selectedPaymentMethod = state.selectedItem as? PaymentOptionsItem.SavedPaymentMethod
+        assertThat(selectedPaymentMethod?.paymentMethod).isEqualTo(paymentMethod)
+    }
+
+    @Test
+    fun `Returns no payment selection if the current selection is no longer available`() {
+        val paymentMethods = PaymentMethodFixtures.createCards(3)
+
+        val state = PaymentOptionsStateFactory.create(
+            paymentMethods = paymentMethods,
+            showGooglePay = false,
+            showLink = false,
+            currentSelection = PaymentSelection.Link,
+            nameProvider = { it!!.resolvableString },
+            isCbcEligible = false,
+            canRemovePaymentMethods = true,
+            defaultPaymentMethodId = null
+        )
+
+        assertThat(state.selectedItem).isNull()
+    }
+
+    @Test
+    fun `'isModifiable' is true when multiple networks are available & is CBC eligible`() {
+        val paymentMethods = PaymentMethodFixtures.createCards(3).toMutableList()
+
+        val lastPaymentMethodWithNetworks = paymentMethods.removeLast().let { paymentMethod ->
+            paymentMethod.copy(
+                card = paymentMethod.card?.copy(
+                    networks = PaymentMethod.Card.Networks(
+                        available = setOf("visa", "cartes_bancaires")
+                    )
+                )
+            )
+        }
+
+        paymentMethods.add(lastPaymentMethodWithNetworks)
+
+        val state = PaymentOptionsStateFactory.create(
+            paymentMethods = paymentMethods,
+            showGooglePay = false,
+            showLink = false,
+            currentSelection = PaymentSelection.Link,
+            nameProvider = { it!!.resolvableString },
+            isCbcEligible = true,
+            canRemovePaymentMethods = true,
+            defaultPaymentMethodId = null
+        )
+
+        assertThat(
+            (state.items.last() as PaymentOptionsItem.SavedPaymentMethod).isModifiable
+        ).isTrue()
+    }
+
+    @Test
+    fun `isEnabledDuringEditing is true for all saved payment methods when remove is enabled`() {
+        testIsEnabledDuringEditingTrueForAllSavedPmsWhenRemoveIsEnabled(useUpdatePaymentMethodScreen = false)
+    }
+
+    @Test
+    fun `isEnabledDuringEditing is true for all saved payment methods when remove is enabled, using updatePM screen`() {
+        testIsEnabledDuringEditingTrueForAllSavedPmsWhenRemoveIsEnabled(useUpdatePaymentMethodScreen = true)
+    }
+
+    private fun testIsEnabledDuringEditingTrueForAllSavedPmsWhenRemoveIsEnabled(useUpdatePaymentMethodScreen: Boolean) {
+        featureFlagTestRule.setEnabled(useUpdatePaymentMethodScreen)
+        val state = createPaymentOptionsState(
+            paymentMethods = PaymentMethodFixtures.createCards(3),
+            canRemovePaymentMethods = true,
+        )
+
+        val options = state.items.filterIsInstance<PaymentOptionsItem.SavedPaymentMethod>()
+
+        assertThat(options[1].isEnabledDuringEditing).isTrue()
+        assertThat(options[2].isEnabledDuringEditing).isTrue()
+        assertThat(options[2].isEnabledDuringEditing).isTrue()
+    }
+
+    @Test
+    fun `canRemovePaymentMethods is false for all saved payment methods when not using updatePM screen`() {
+        featureFlagTestRule.setEnabled(false)
+        val state = createPaymentOptionsState(
+            paymentMethods = PaymentMethodFixtures.createCards(3),
+            canRemovePaymentMethods = false,
+        )
+
+        val options = state.items.filterIsInstance<PaymentOptionsItem.SavedPaymentMethod>()
+
+        assertThat(options[0].isEnabledDuringEditing).isFalse()
+        assertThat(options[1].isEnabledDuringEditing).isFalse()
+        assertThat(options[2].isEnabledDuringEditing).isFalse()
+    }
+
+    @Test
+    fun `canRemovePaymentMethods is false for all saved payment methods unless can modify`() {
+        featureFlagTestRule.setEnabled(false)
+        val paymentMethods = PaymentMethodFixtures.createCards(3).run {
+            val mutablePaymentMethods = toMutableList()
+            val updatedPaymentMethod = mutablePaymentMethods[2].run {
+                copy(
+                    card = card?.copy(
+                        networks = PaymentMethod.Card.Networks(
+                            available = setOf("visa", "cartes_bancaires")
+                        )
+                    )
+                )
+            }
+
+            mutablePaymentMethods[2] = updatedPaymentMethod
+
+            mutablePaymentMethods
+        }
+
+        val state = createPaymentOptionsState(
+            paymentMethods = paymentMethods,
+            canRemovePaymentMethods = false,
+        )
+
+        val options = state.items.filterIsInstance<PaymentOptionsItem.SavedPaymentMethod>()
+
+        assertThat(options[0].isEnabledDuringEditing).isFalse()
+        assertThat(options[1].isEnabledDuringEditing).isFalse()
+        assertThat(options[2].isEnabledDuringEditing).isTrue()
+    }
+
+    @Test
+    fun `saved payment methods are enabled even when cannot remove when using updatePM screen`() {
+        featureFlagTestRule.setEnabled(true)
+        val state = createPaymentOptionsState(
+            paymentMethods = PaymentMethodFixtures.createCards(3),
+            canRemovePaymentMethods = false,
+        )
+
+        val options = state.items.filterIsInstance<PaymentOptionsItem.SavedPaymentMethod>()
+
+        assertThat(options[0].isEnabledDuringEditing).isTrue()
+        assertThat(options[1].isEnabledDuringEditing).isTrue()
+        assertThat(options[2].isEnabledDuringEditing).isTrue()
+    }
+
+    @Test
+    fun `State is correct when given non null defaultPaymentMethodId`() {
+        val paymentMethods = PaymentMethodFixtures.createCards(3)
+
+        val defaultPaymentMethod = paymentMethods[0]
+        val defaultPaymentMethodId = defaultPaymentMethod.id
+
+        assertThat(defaultPaymentMethodId).isNotNull()
+
+        val state = PaymentOptionsStateFactory.create(
+            paymentMethods = paymentMethods,
+            showGooglePay = true,
+            showLink = true,
+            currentSelection = PaymentSelection.Saved(defaultPaymentMethod),
+            nameProvider = { it!!.resolvableString },
+            isCbcEligible = false,
+            canRemovePaymentMethods = true,
+            defaultPaymentMethodId = defaultPaymentMethodId
+        )
+
+        val options = state.items.filterIsInstance<PaymentOptionsItem.SavedPaymentMethod>()
+
+        assertThat(options[0].paymentMethod.id).isEqualTo(defaultPaymentMethodId)
+        assertThat(options[0].displayableSavedPaymentMethod.shouldShowDefaultBadge).isTrue()
+        assertThat(options[1].displayableSavedPaymentMethod.shouldShowDefaultBadge).isFalse()
+        assertThat(options[2].displayableSavedPaymentMethod.shouldShowDefaultBadge).isFalse()
+    }
+
+    private fun createPaymentOptionsState(
+        paymentMethods: List<PaymentMethod>,
+        canRemovePaymentMethods: Boolean,
+    ): PaymentOptionsState {
+        return PaymentOptionsStateFactory.create(
+            paymentMethods = paymentMethods,
+            showGooglePay = false,
+            showLink = false,
+            currentSelection = PaymentSelection.Link,
+            nameProvider = { it!!.resolvableString },
+            isCbcEligible = true,
+            canRemovePaymentMethods = canRemovePaymentMethods,
+            defaultPaymentMethodId = null
+        )
+    }
+}
---
+++ stripe-attestation/src/main/java/com/stripe/attestation/IntegrityStandardRequestManager.kt
@@ -0,0 +1,145 @@
+package com.stripe.attestation
+
+import androidx.annotation.RestrictTo
+import com.google.android.play.core.integrity.StandardIntegrityManager
+import com.google.android.play.core.integrity.StandardIntegrityManager.PrepareIntegrityTokenRequest
+import com.google.android.play.core.integrity.StandardIntegrityManager.StandardIntegrityTokenProvider
+import com.google.android.play.core.integrity.StandardIntegrityManager.StandardIntegrityTokenRequest
+import kotlinx.coroutines.delay
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+interface IntegrityRequestManager {
+    /**
+     * Prepare the integrity token. This warms up the integrity token generation, it's recommended
+     * to call it as soon as possible if you know you will need an integrity token.
+     *
+     * @param maxRetries The number of times to retry the request (using exponential backoff).
+     * Increase this value if calls to this method are non-blocking.
+     * See https://developer.android.com/google/play/integrity/error-codes#retry-logic
+     *
+     * Needs to be called before calling [requestToken].
+     */
+    suspend fun prepare(
+        maxRetries: Int = 0,
+    ): Result<Unit>
+
+    /**
+     * Requests an Integrity token.
+     *
+     * @param requestIdentifier A string to be hashed to generate a request identifier.
+     * Can be null. Provide a value that identifies the API request
+     * to protect it from tampering attacks.
+     * @param maxRetries The number of times to retry the request (using exponential backoff).
+     * Increase this value if calls to this method are non-blocking.
+     * See https://developer.android.com/google/play/integrity/error-codes#retry-logic
+     *
+     *  [Docs](https://developer.android.com/google/play/integrity/standard#protect-requests)
+     */
+    suspend fun requestToken(
+        requestIdentifier: String? = null,
+        maxRetries: Int = 0,
+    ): Result<String>
+}
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+class IntegrityStandardRequestManager(
+    private val cloudProjectNumber: Long,
+    private val logError: (String, Throwable) -> Unit,
+    private val factory: StandardIntegrityManagerFactory
+) : IntegrityRequestManager {
+
+    private val standardIntegrityManager: StandardIntegrityManager by lazy { factory.create() }
+    private var integrityTokenProvider: StandardIntegrityTokenProvider? = null
+
+    override suspend fun prepare(
+        retries: Int,
+    ): Result<Unit> = exponentialBackoff(maxRetries = retries) {
+        runCatching {
+            val finishedTask = standardIntegrityManager
+                .prepareIntegrityToken(
+                    PrepareIntegrityTokenRequest.builder()
+                        .setCloudProjectNumber(cloudProjectNumber)
+                        .build()
+                ).awaitTask()
+
+            finishedTask.toResult()
+                .onSuccess { integrityTokenProvider = it }
+                .getOrThrow()
+        }
+            .map {}
+            .recoverCatching {
+                logError("Integrity - Failed to prepare integrity token", it)
+                throw AttestationError.fromException(it)
+            }
+    }
+
+    override suspend fun requestToken(
+        requestIdentifier: String?,
+        maxRetries: Int,
+    ): Result<String> = request(requestIdentifier, maxRetries)
+
+    private suspend fun request(
+        requestHash: String?,
+        maxRetries: Int,
+    ): Result<String> = exponentialBackoff(
+        maxRetries = maxRetries,
+    ) {
+        runCatching {
+            val finishedTask = requireNotNull(
+                value = integrityTokenProvider,
+                lazyMessage = { "Integrity token provider is not initialized. Call prepare() first." }
+            ).request(
+                StandardIntegrityTokenRequest.builder()
+                    .setRequestHash(requestHash)
+                    .build()
+            ).awaitTask()
+
+            finishedTask.toResult().getOrThrow()
+        }
+            .map { it.token() }
+            .recoverCatching {
+                logError("Integrity - Failed to prepare integrity token", it)
+                throw AttestationError.fromException(it)
+            }
+    }
+
+    suspend fun <T> exponentialBackoff(
+        maxRetries: Int,
+        initialDelay: Long = INITIAL_DELAY,
+        block: suspend () -> Result<T>
+    ): Result<T> {
+        var currentDelay = initialDelay
+        val totalTries = maxRetries + 1
+        repeat(totalTries) { attempt ->
+            val result = block()
+
+            if (result.isSuccess) {
+                return result
+            }
+
+            val exception = result.exceptionOrNull()
+
+            // Retry only if the error is retriable
+            if (exception is AttestationError && exception.errorType.isRetriable) {
+                logError("Retrying due to retriable error on attempt $attempt", exception)
+                delay(currentDelay)
+                currentDelay = (currentDelay * MULTIPLIER).toLong()
+            } else {
+                return result
+            }
+        }
+        return Result.failure<T>(
+            AttestationError(
+                errorType = AttestationError.ErrorType.MAX_RETRIES_EXCEEDED,
+                message = "Failed after $maxRetries attempts, giving up.",
+                cause = null
+            )
+        )
+    }
+
+    companion object {
+        // Constants for the retry mechanism
+        private const val INITIAL_DELAY = 2000L // Start with 2 seconds
+        private const val MULTIPLIER = 2.0
+    }
+}
