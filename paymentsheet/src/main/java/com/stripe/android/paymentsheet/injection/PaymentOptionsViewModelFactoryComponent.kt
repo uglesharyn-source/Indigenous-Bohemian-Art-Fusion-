4934eaf8893d7bcf6b7a4b71afa21f1cb31371c7
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 02:50:17 +0000

    Initialize tolu/uspm/card_update_params

---
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/injection/PaymentOptionsViewModelFactoryComponent.kt
@@ -0,0 +1,48 @@
+package com.stripe.android.paymentsheet.injection
+
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.SavedStateHandle
+import com.stripe.android.core.injection.CoreCommonModule
+import com.stripe.android.core.injection.CoroutineContextModule
+import com.stripe.android.payments.core.injection.PRODUCT_USAGE
+import com.stripe.android.payments.core.injection.StripeRepositoryModule
+import com.stripe.android.ui.core.di.CardScanModule
+import com.stripe.android.ui.core.forms.resources.injection.ResourceRepositoryModule
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        StripeRepositoryModule::class,
+        PaymentSheetCommonModule::class,
+        PaymentOptionsViewModelModule::class,
+        CoroutineContextModule::class,
+        CoreCommonModule::class,
+        ResourceRepositoryModule::class,
+        CardScanModule::class
+    ]
+)
+internal interface PaymentOptionsViewModelFactoryComponent {
+    val paymentOptionsViewModelSubcomponentBuilder: PaymentOptionsViewModelSubcomponent.Builder
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun context(context: Context): Builder
+
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        @BindsInstance
+        fun savedStateHandle(handle: SavedStateHandle): Builder
+
+        @BindsInstance
+        fun productUsage(@Named(PRODUCT_USAGE) productUsage: Set<String>): Builder
+
+        fun build(): PaymentOptionsViewModelFactoryComponent
+    }
+}
