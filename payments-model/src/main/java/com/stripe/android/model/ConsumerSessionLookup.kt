11a77e534cb9a5d83baea70ff083d32873c1eb71
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 01:20:14 +0000

    Initialize changelog/20.50.2

---
+++ payments-model/src/main/java/com/stripe/android/model/ConsumerSessionLookup.kt
@@ -0,0 +1,24 @@
+package com.stripe.android.model
+
+import androidx.annotation.RestrictTo
+import com.stripe.android.core.model.StripeModel
+import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * The result of a call to retrieve the [ConsumerSession] for a Link user.
+ */
+@Parcelize
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+@Serializable
+data class ConsumerSessionLookup(
+    @SerialName("exists")
+    val exists: Boolean,
+    @SerialName("consumer_session")
+    val consumerSession: ConsumerSession? = null,
+    @SerialName("error_message")
+    val errorMessage: String? = null,
+    @SerialName("publishable_key")
+    val publishableKey: String? = null,
+) : StripeModel
