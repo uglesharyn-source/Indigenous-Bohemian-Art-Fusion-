5a0fd2befaae3dacf8c73399dd97d74ae1168a26
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 01:35:22 +0000

    Initialize carlosmuvi/BANKCON-16095/android-update-cached-accounts-after-skip-account

---
+++ link/src/main/java/com/stripe/android/link/LinkActivityViewModel.kt
@@ -0,0 +1,62 @@
+package com.stripe.android.link
+
+import android.app.Application
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.navigation.NavHostController
+import com.stripe.android.link.LinkActivity.Companion.getArgs
+import com.stripe.android.link.injection.DaggerNativeLinkComponent
+import com.stripe.android.link.injection.NativeLinkComponent
+import javax.inject.Inject
+
+internal class LinkActivityViewModel @Inject constructor(
+    val activityRetainedComponent: NativeLinkComponent
+) : ViewModel() {
+    var navController: NavHostController? = null
+    var dismissWithResult: ((LinkActivityResult) -> Unit)? = null
+
+    fun handleViewAction(action: LinkAction) {
+        when (action) {
+            LinkAction.BackPressed -> handleBackPressed()
+        }
+    }
+
+    private fun handleBackPressed() {
+        navController?.let { navController ->
+            if (!navController.popBackStack()) {
+                dismissWithResult?.invoke(LinkActivityResult.Canceled())
+            }
+        }
+    }
+
+    fun unregisterActivity() {
+        navController = null
+        dismissWithResult = null
+    }
+
+    companion object {
+        fun factory(savedStateHandle: SavedStateHandle? = null): ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val handle: SavedStateHandle = savedStateHandle ?: createSavedStateHandle()
+                val app = this[APPLICATION_KEY] as Application
+                val args: NativeLinkArgs = getArgs(handle) ?: throw NoArgsException()
+
+                DaggerNativeLinkComponent
+                    .builder()
+                    .configuration(args.configuration)
+                    .publishableKeyProvider { args.publishableKey }
+                    .stripeAccountIdProvider { args.stripeAccountId }
+                    .context(app)
+                    .build()
+                    .viewModel
+            }
+        }
+    }
+}
+
+internal class NoArgsException : IllegalArgumentException("NativeLinkArgs not found")
