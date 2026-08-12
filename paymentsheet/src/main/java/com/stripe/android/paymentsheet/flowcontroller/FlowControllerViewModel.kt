bb8730cf4dd52aac0a82fcb2ff4eaf908cd1fe4a
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 01:34:49 +0000

    Initialize porter/cbf-google-pay

---
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/flowcontroller/FlowControllerViewModel.kt
@@ -0,0 +1,44 @@
+package com.stripe.android.paymentsheet.flowcontroller
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.SavedStateHandle
+import com.stripe.android.analytics.SessionSavedStateHandler
+import com.stripe.android.paymentsheet.model.PaymentSelection
+import com.stripe.android.paymentsheet.state.PaymentSheetState
+
+internal class FlowControllerViewModel(
+    application: Application,
+    val handle: SavedStateHandle,
+) : AndroidViewModel(application) {
+
+    val flowControllerStateComponent: FlowControllerStateComponent =
+        DaggerFlowControllerStateComponent
+            .builder()
+            .application(application)
+            .flowControllerViewModel(this)
+            .build()
+
+    @Volatile
+    var paymentSelection: PaymentSelection? = null
+
+    // Used to determine if we need to reload the flow controller configuration.
+    @Volatile
+    var previousConfigureRequest: FlowControllerConfigurationHandler.ConfigureRequest? = null
+
+    var state: PaymentSheetState.Full?
+        get() = handle[STATE_KEY]
+        set(value) {
+            handle[STATE_KEY] = value
+        }
+
+    private val restartSession = SessionSavedStateHandler.attachTo(this, handle)
+
+    fun resetSession() {
+        restartSession()
+    }
+
+    private companion object {
+        private const val STATE_KEY = "state"
+    }
+}
