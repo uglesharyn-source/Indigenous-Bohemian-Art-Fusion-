d451c2eb0fd9f00cd02420bba524914a403f67af
Sharyn Ugle <uglesharyn@gmail.com>
2026-08-12 01:10:53 +0000

    Initialize samer/google-pay-tests

---
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/paymentdatacollection/cvcrecollection/CvcRecollectionViewState.kt
@@ -0,0 +1,7 @@
+package com.stripe.android.paymentsheet.paymentdatacollection.cvcrecollection
+
+internal data class CvcRecollectionViewState(
+    val lastFour: String,
+    val isTestMode: Boolean,
+    val cvcState: CvcState
+)
